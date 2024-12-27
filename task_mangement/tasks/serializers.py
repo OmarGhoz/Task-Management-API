50640a952bb591049a425116c5bcea6a4d0c58d5
OmarGhoz <omarghoz999@gmail.com>
2024-12-27 22:28:31 +0200

    Task Management API

--- task_mangement/tasks/serializers.py
+++ task_mangement/tasks/serializers.py
@@ -4,7 +4,6 @@ from django.contrib.auth.hashers import make_password
 from django.utils.timezone import now
 
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
